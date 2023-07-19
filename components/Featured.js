0ea29916be987cae14e3d18599977f588adea0fd
charafimarouane <charafimarouane007@gmail.com>
2023-07-19 16:20:49 +0000

    setting hero

--- components/Featured.js
+++ components/Featured.js
@@ -8,7 +8,7 @@ export default function Featured({}){
                 
                 <h1 className="md:text-7xl text-5xl mb-4">Summer outfits</h1>
                 <Link href={"/"} className="">
-                    <h1 className="md:text-3xl hover:md:bg-secoundary rounded-md md:border-2 md:border-secoundary bg-secoundary md:bg-white md:text-secoundary text-white md:ml-0 mx-auto hover:md:text-white py-2 px-3 w-fit md:mt-5">Available now!</h1>
+                    <h1 className="md:text-3xl text-xl hover:md:bg-secoundary rounded-md md:border-2 md:border-secoundary bg-secoundary md:bg-white md:text-secoundary text-white md:ml-0 mx-auto hover:md:text-white py-2 px-3 w-fit md:mt-5">Available now!</h1>
                 </Link>
             </div>
             <div  className="md:w-1/2 mx-auto px-8">
